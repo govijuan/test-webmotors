20834a48b2899c12cdb01f5966dbb063ac2feccc
Juan Carlos Coral <govindasd@gmail.com>
2020-06-20 19:29:49 -0300

    Setting up primary structure for the app

---
+++ src/components/pages/SearchPage/index.jsx
@@ -0,0 +1,15 @@
+import React, {Component} from 'react'
+
+class SearchPage extends Component {
+
+    render(){
+        return(
+            <div>
+                <h1>Página de Busca</h1>
+            </div>
+        )
+    }
+
+}
+
+export default SearchPage
