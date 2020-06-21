f9f065e2b5062eaa7d92e3db011df74e0f8b06b8
Juan Carlos Coral <govindasd@gmail.com>
2020-06-21 18:44:11 -0300

    Adding the header with styles for desktop

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
 import SearchPage from './components/pages/SearchPage';
+import Header from './components/organisms/Header';
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <SearchPage />
     </div>
   );
